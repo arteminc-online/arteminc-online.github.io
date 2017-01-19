72403e11c6fd546cb735eb16b0848dea8721a880
arteminc <contact.arteminc@gmail.com>
2017-01-18 18:31:45 -0800

    Create menuBar.js

---
+++ menuBar.js
@@ -0,0 +1,8 @@
+var isSignedIn = localStorage.getItem("isSignedIn");
+var currentUser = localStorage.getItem("currentUser");
+document.getElementById("greeting").innerHTML = "Hi " + currentUser + "!";
+function logOut () {
+  localStorage.setItem("isSignedIn","false");
+  localStorage.setItem("currentUser", "none");
+  window.location.replace("index.html");
+}
